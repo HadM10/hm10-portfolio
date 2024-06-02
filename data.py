cd4000e55f4dcd9e3e889e429b8abf68d0149b3d
HadM10 <hadmak10@gmail.com>
2024-06-02 22:09:54 +0000

    all

---
+++ data.py
@@ -0,0 +1,158 @@
+my_data = {
+    "name": "Hadi",
+    "full_name": "Hadi Youssef Makki",
+    "title": "Full Stack Developer/Mobile Developer",
+    "slogan": "Let's wave with creativity",
+    "cv_pdf": "cv.pdf",
+    "about": {
+        "1": "I am a dedicated full-stack and mobile developer with a passion for creating seamless digital experiences. My journey in technology began with a deep fascination for computer science, which led me to pursue a degree in Computer Science at the Lebanese International University (LIU). My academic foundation, coupled with hands-on experience, has equipped me with a robust understanding of both front-end and back-end development. ",
+        "2": "After my formal education, I further honed my skills by completing a five-month, full-time bootcamp at ESA Coding Lab, ESIEE IT, and ESA Business School. During this intensive program, I became proficient in the MERN stack and acquired a broad array of technologies that have become essential tools in my development toolkit. This experience not only solidified my technical expertise but also instilled a strong sense of discipline and adaptability.",
+        "3": "I thrive in collaborative environments where innovation and creativity are at the forefront. My ability to communicate effectively and adapt quickly ensures that I can contribute meaningfully to any team, driving projects to successful outcomes. While I am fully capable of working independently, I believe that being part of a professional team can accelerate my career and amplify my impact.",
+        "4": "Outside the realm of technology, I am a music composer and piano player. Music serves as a source of inspiration and creativity, allowing me to approach problem-solving with a unique perspective. The harmonies and rhythms of music often translate into innovative ideas in my development work, fostering a continuous cycle of creativity and technical excellence."
+    },
+    "about_tasks": [
+        {
+            "data_animate_effect": "fadeInLeft",
+            "icon": "fas fa-code",
+            "title": "Frontend Developer"
+        },
+        {
+            "data_animate_effect": "fadeInRight",
+            "icon": "fas fa-mobile-alt",
+            "title": "Mobile Developer"
+        },
+        {
+            "data_animate_effect": "fadeInTop",
+            "icon": "fas fa-server",
+            "title": "Backend Developer"
+        },
+        {
+            "data_animate_effect": "fadeInBottom",
+            "icon": "fas fa-music",
+            "title": "Music Composer/Piano Player"
+        }
+    ],
+    "releases": [
+        "Four Websites [Design, Backend, Frontend]",
+        "2 Mobile App, one of them on Google Play Store",
+        "Tank War Game on Unity",
+        "200+ Music Pieces Played"
+    ],
+    "what_i_do": [
+    {
+        "icon": "fas fa-lightbulb",
+        "title": "Innovative Solutions",
+        "details": "Bringing creative and practical approaches to solving complex problems in web and mobile development."
+    },
+    {
+        "icon": "fas fa-code",
+        "title": "Full Stack Development",
+        "details": "Expert in developing both client and server software using technologies like HTML, CSS, JavaScript, MERN Stack, PHP, SQL."
+    },
+    {
+        "icon": "fas fa-server",
+        "title": "Backend Development",
+        "details": "Proficient in creating robust backend systems with a focus on APIs, databases, and server-side logic using PHP and SQL."
+    },
+    {
+        "icon": "fas fa-mobile-alt",
+        "title": "Mobile Development",
+        "details": "Building responsive and user-friendly mobile applications using Flutter for both iOS and Android platforms."
+    },
+    {
+        "icon": "fas fa-layer-group",
+        "title": "UI/UX Design",
+        "details": "Designing intuitive user interfaces and enhancing user experience through thoughtful design and usability principles."
+    },
+    {
+        "icon": "fas fa-music",
+        "title": "Music Composition",
+        "details": "Composing music and playing the piano, blending creativity with technical expertise to produce harmonious music."
+    }
+],
+    "stats": [
+        {
+            "title": "Projects",
+            "total": "10"
+        },
+      
+    ],
+    "experience_years": 4,
+    "skills": [
+          {
+            "title": "HTML5, CSS3, JS, Ajax, Json",
+            "data_animation_effect": "fadeInRight",
+            "level": 95
+        },
+        {
+            "title": "PHP, SQL, Git",
+            "data_animation_effect": "fadeInLeft",
+            "level": 95
+        },
+        {
+            "title": "Flutter",
+            "data_animation_effect": "fadeInLeft",
+            "level": 90
+        },
+        {
+            "title": "React.js",
+            "data_animation_effect": "fadeInRight",
+            "level": 80
+        },
+        {
+            "title": "Node.js, Express.js, MongoDB",
+            "data_animation_effect": "fadeInRight",
+            "level": 80
+        }
+    ],
+    "education": [
+        {
+            "position": "One",
+            "title": "Bachelor of Science in Computer Science",
+            "location": "Lebanese International University (LIU)",
+            "starting": "2020",
+            "ending": "2023"
+        },
+        {
+            "position": "Two",
+            "title": "Full Stack Developer Diploma from ESIEE IT and ESA Business School",
+            "location": "ESA Coding Lab Boot camp",
+            "starting": "2020",
+            "ending": "2021"
+        },
+    ],
+   "work": [
+    {
+        "title": "Co-founder and Lead Developer at Top Coders",
+        "starting": "2023",
+        "ending": "Present",
+        "content": [
+            'Developed the Village Guide Mobile App and Website called "Ansar Portal" using Flutter, enabling users to search, like, discover stores, news, offers, and more in the village Ansar.',
+        ],
+        "link": "https://Topcoders.com"
+    },
+    {
+        "title": "Freelance Full Stack Developer",
+        "starting": "2019",
+        "ending": "Present",
+        "content": [
+            "Created and maintained responsive websites and mobile apps using various technologies.",
+            "Developed a fully functional E-commerce website using HTML, CSS, JavaScript, JQuery, PHP, Ajax, and SQL.",
+            "Designed and implemented an Anime Website for searching, watching, and managing favorites using HTML, CSS, JQuery, and JavaScript.",
+            "Managed the maintenance of websites upon request and used databases such as PostgreSQL and SQLite.",
+            "Utilized Jetbrains tools (Pycharm), GitHub, Heroku, and Azure portal.",
+             "Designed and implemented an E-learning Website using the MERN Stack (React, Express, Node, MongoDB) for private tutor services.",
+            "Created a Cinema Booking Website with functionalities for booking movies, selecting dates, times, and seats, managing profiles, and checking out using the MERN Stack (React, Express, Node, MongoDB)."
+        ],
+        "link": "https://akatsukishoponline.000webhostapp.com"
+    }
+],
+
+    "contacts": {
+        "email": "hadmak20@gmail.com",
+        # "pro_email": "ltkworksoft@gmail.com",
+        "phone": "+961 70 836 770",
+        "github": "https://github.com/HadM10",
+        "linkedin": "https://www.linkedin.com/in/hadi-makki-88b6b8212"
+    }
+}
